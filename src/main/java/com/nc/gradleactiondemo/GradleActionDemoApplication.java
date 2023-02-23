e26dcded660eb9e0e1908ff24976f602e7f66b74
H18nikhil <36754393+H18nikhil@users.noreply.github.com>
2023-02-23 20:09:14 +0530

    Initial commit

---
+++ src/main/java/com/nc/gradleactiondemo/GradleActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nc.gradleactiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleActionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nc/gradleactiondemo/GradleActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc.gradleactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
